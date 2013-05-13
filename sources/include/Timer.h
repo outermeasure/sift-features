ffd1a48e7e96452bb7df3eb11bfb66d9d47825e8
Alexandru Pana <alex.pana@vertexarmy.org>
2013-05-13 19:23:47 +0100

    Refactored Timer

--- sources/include/Timer.h
+++ sources/include/Timer.h
@@ -24,16 +24,16 @@ public:
 		switch ( unit )
 		{
 		case SECONDS:
-			return duration_cast<std::chrono::seconds>(now - startTime).count();
+			return (int)duration_cast<std::chrono::seconds>(now - startTime).count();
 		case MILISECONDS:
-			return duration_cast<std::chrono::milliseconds>(now - startTime).count();
+			return (int)duration_cast<std::chrono::milliseconds>(now - startTime).count();
 		case MICROSECONDS:
-			return duration_cast<std::chrono::microseconds>(now - startTime).count();
+			return (int)duration_cast<std::chrono::microseconds>(now - startTime).count();
 		case NANOSECONDS:
-			return duration_cast<std::chrono::nanoseconds>(now - startTime).count();
+			return (int)duration_cast<std::chrono::nanoseconds>(now - startTime).count();
 
 		default:
-			return duration_cast<seconds>(now - startTime).count();
+			return (int)duration_cast<seconds>(now - startTime).count();
 		}
 		return -1;
 	}
@@ -42,13 +42,6 @@ public:
 		Reset();
 	}
 
-	static Timer GlobalInstance(){
-		return globalInstance;
-	}
-
 private:
-	static Timer globalInstance;
 	std::chrono::high_resolution_clock::time_point startTime;
-
-	// start time
 };
